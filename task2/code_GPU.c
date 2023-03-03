8a9e76da9a1e6b2db27faa8e17fa90e0286b053e
psan3333 <59166366+psan3333@users.noreply.github.com>
2023-03-03 18:34:27 +0700

    Update code_GPU.c

--- task2/code_GPU.c
+++ task2/code_GPU.c
@@ -8,12 +8,9 @@ int main(int argc, char* argv[]) {
     int size = atoi(argv[2]);
 
     double** A = (double**)malloc(size * sizeof(double*));
-    for (int i = 0; i < size; i++) {
-        A[i] = (double*)calloc(size, sizeof(double));
-    }
-
     double** Anew = (double**)malloc(size * sizeof(double*));
     for (int i = 0; i < size; i++) {
+        A[i] = (double*)calloc(size, sizeof(double));
         Anew[i] = (double*)calloc(size, sizeof(double));
     }
 
@@ -35,49 +32,53 @@ int main(int argc, char* argv[]) {
     A[size - 1][size - 1] = 20.0;
     A[0][size - 1] = 30.0;
     A[0][0] = 20.0;
+    Anew[size - 1][0] = 10.0;
+    Anew[size - 1][size - 1] = 20.0;
+    Anew[0][size - 1] = 30.0;
+    Anew[0][0] = 20.0;
 
-    for (int i = 1; i < size - 1; i++) {
-        A[size - 1][i] = A[size - 1][0] + (double)10.0 * i / (size - 1);
-    }
 
-    for (int i = size - 2; i > 0; i--) {
-        A[i][size - 1] = A[size  - 1][size - 1] + (double)10.0 * (size - i - 1) / (size - 1);
-    }
-
-    for (int i = 1; i < size - 1; i++){
-        A[0][i] = A[0][0] + (double)10.0 * i / (size - 1);
-    }
-
-    for (int i = size - 2; i > 0; i--) {
-        A[i][0] = A[size - 1][0] + (double)10.0 * (size - i - 1) / (size - 1);
-    }
-
-    #pragma acc data copy(A[0:size][0:size]) create(Anew[0:size][0:size]) 
-    while (error > precision && iter < max_iter_input) {
-
-        error = 0.0;
-        ++iter;
-
-        #pragma acc parallel loop reduction(max:error)
-        for (int j = 1; j < size - 1; j++){
-            #pragma acc loop reduction(max:error)
-            for (int i = 1; i < size - 1; i++) {
-                Anew[i][j] = 0.25 * ((A[i + 1][j] + A[i - 1][j]) + (A[i][j - 1] + A[i][j + 1]));
-                error = fmax( error, fabs(Anew[j][i] - A[j][i]));
-            }
-        }
-
+    #pragma acc data copy(A[0:size][0:size], Anew[0:size][0:size]) 
+    {
         #pragma acc parallel loop gang worker num_workers(4) vector_length(32)
         for (int i = 1; i < size - 1; i++) {
-            #pragma acc loop vector
-            for (int j = 1; j < size - 1; j++) {
-                A[i][j] = Anew[i][j];
+            A[size - 1][i] = A[size - 1][0] + (double)10.0 * i / (size - 1);
+            Anew[size - 1][i] = A[size - 1][i];
+
+            A[0][i] = A[0][0] + (double)10.0 * i / (size - 1);
+            Anew[0][i] = A[0][i];
+
+            A[size - 1 - i][size - 1] = A[size  - 1][size - 1] + (double)10.0 * i / (size - 1);
+            Anew[size - 1 - i][size - 1] = A[size - 1 - i][size - 1];
+
+            A[size - 1 - i][0] = A[size - 1][0] + (double)10.0 * i / (size - 1);
+            Anew[size - 1 - i][0] = A[size - 1 - i][0];
+        }
+
+        while (error > precision && iter < max_iter_input) {
+
+            error = 0.0;
+
+            ++iter;
+
+            #pragma acc parallel loop gang worker num_workers(4) vector_length(32) reduction(max:error)
+            for (int j = 1; j < size - 1; j++){
+                #pragma acc loop vector reduction(max:error)
+                for (int i = 1; i < size - 1; i++) {
+                    if (iter % 2) {
+                        Anew[i][j] = 0.25 * (A[i + 1][j] + A[i - 1][j] + A[i][j - 1] + A[i][j + 1]);
+                    }
+                    else {
+                        A[i][j] = 0.25 * (Anew[i + 1][j] + Anew[i - 1][j] + Anew[i][j - 1] + Anew[i][j + 1]);
+                    }
+                    error = fmax(error, fabs(Anew[j][i] - A[j][i]));
+                }
             }
         }
+
+        printf("%d %lf\n", iter, error);
     }
     
-    printf("%d %lf\n", iter, error);
-
     for (int i = 0; i < size; i++) {
         free(A[i]);
         free(Anew[i]);
