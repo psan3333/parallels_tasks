1af8dee9f0acf8ffd25103c6cb1238cf7737bdfa
psan3333 <59166366+psan3333@users.noreply.github.com>
2023-05-27 20:34:37 +0700

    Update code.cu

--- task5/code.cu
+++ task5/code.cu
@@ -28,13 +28,13 @@ double 	*A 		= nullptr,  // buffer for main matrix
 void free_pointers()
 {
 	std::cout << "End of execution" << std::endl;
-	if (A) 	            cudaFree(A); CUDACHECK()
-	if (Anew) 	    cudaFree(Anew); CUDACHECK()
-	if (dev_A)	    cudaFree(dev_A); CUDACHECK()
-	if (dev_Anew) 	    cudaFree(dev_Anew); CUDACHECK()
-    	if (buff)           cudaFree(buff); CUDACHECK()
-	if (d_out) 	    cudaFree(d_out); CUDACHECK()
-	if (d_temp_storage) cudaFree(d_temp_storage); CUDACHECK()
+	if (A) 	            cudaFree(A); CUDACHECK("free A")
+	if (Anew) 	    cudaFree(Anew); CUDACHECK("free Anew")
+	if (dev_A)	    cudaFree(dev_A); CUDACHECK("free dev_A")
+	if (dev_Anew) 	    cudaFree(dev_Anew); CUDACHECK("free dev_Anew")
+    	if (buff)           cudaFree(buff); CUDACHECK("free buff")
+	if (d_out) 	    cudaFree(d_out); CUDACHECK("free d_out")
+	if (d_temp_storage) cudaFree(d_temp_storage); CUDACHECK("free d_temp_storage")
 	std::cout << "Memory has been freed" << std::endl;
 	// end MPI engine
 	int code = MPI_Finalize();
